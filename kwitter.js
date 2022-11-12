a910ef56f5aa003bb0bfa068da37a46093930baf
mateito135798 <112591572+mateito135798@users.noreply.github.com>
2022-11-11 18:10:58 -0600

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,7 @@
+function w() {
+var e=document.getElementById("q").value;
+localStorage.setItem("user",e);
+window.location="kwitter_room.html"
+
+
+}
---
+++ kwitter_room.js
@@ -0,0 +1,48 @@
+var firebaseConfig = {
+      apiKey: "AIzaSyBidAEGvMse9kBgKy-CD9XJzQ4zRMQ5Noc",
+      authDomain: "hombre-dc0c2.firebaseapp.com",
+      databaseURL: "https://hombre-dc0c2-default-rtdb.firebaseio.com",
+      projectId: "hombre-dc0c2",
+
+      storageBucket: "hombre-dc0c2.appspot.com",
+      messagingSenderId: "592908258735",
+      appId: "1:592908258735:web:da4a2fd01fcca348c7f35a"
+    };    
+firebase.initializeApp(firebaseConfig);
+var user=localStorage.getItem("user");
+var hangoutRooms=localStorage.getItem("room");
+document.getElementById("t").innerHTML="hola "+user;
+
+function añadir() {
+var Room=document.getElementById("f").value;
+localStorage.setItem("room",Room);
+firebase.database().ref("/").child(Room).update({
+sala:"nueva"
+});
+
+
+
+      
+}
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Inicio del código
+
+      //Final del código
+      });});}
+getData();
+
+//LLENAR FUNCION ABAJO NO ELIMINAR PORQUE SE QUE SE ME VA A OLVIDAR AUN CON EL RECORDATORIO POR QUE ESTA MUY LARGO :>
+   function volver() {
+      window.location="index.html"
+      localStorage.removeItem("user");
+      localStorage.removeItem("room");
+   }                 
+//|
+//|
+//|
+//|
+//v
+
+
